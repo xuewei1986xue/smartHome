91f0202a1bbd94624ceb3edeaff1383e3b7c2e8e
yzchen <yzchen@smit.com.cn>
2011-12-20 21:12:53 +0800

    init

---
+++ src/cn/com/ehome/InfoActivity.java
@@ -0,0 +1,15 @@
+package cn.com.ehome;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class InfoActivity extends Activity {
+		
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+		 
+		 super.onCreate(savedInstanceState);
+	        setContentView(R.layout.info);
+	 }
+		
+}
